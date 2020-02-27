ca28266cd0e5d9b82512be71d2696fac03ed4e32
SafrazHakamali <safrazhakamali@gmail.com>
2020-02-27 22:39:12 +0800

    A little but not yet MVP

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+	constructor () {
+	    	super()
+	    	this.state = {
+	    		word : "",
+	    		list : ""
+	    	};
+	};
+	changeHandler(event){
+	  this.setState({word:event.target.value});
+	  console.log("change", event.target.value);
+}
   render() {
     return (
       <div>
-        Welcome.
+        {this.state.list}
+        <input onChange={(event) =>{this.changeHandler(event);}}/>
+        <button onClick={() =>{this.setState( { list : this.state.word, value: ""} ) } }>Erase</button>
       </div>
     );
-  }
-}
+  };
+};
 
 export default hot(module)(App);
