4efd7c0eb42ea3aa0066249bdb6014eecdec79d6
SafrazHakamali <safrazhakamali@gmail.com>
2020-03-01 22:11:38 +0800

    Bug that i cant seem to fix

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+
 class App extends React.Component {
-	constructor () {
-	    	super()
-	    	this.state = {
-	    		word : "",
-	    		list : ""
-	    	};
-	};
-	changeHandler(event){
-	  this.setState({word:event.target.value});
-	  console.log("change", event.target.value);
-}
+  constructor(){
+    super()
+    this.state = {
+      word : "",
+      list: []
+    }
+  }
+
+  getInputValue(event){
+    this.setState({word:event.target.value});
+  }
+
+  handleClick(event){
+      let currentTask = this.state.word;
+      let taskArr = this.state.list;
+      taskArr.push(currentTask);
+      let listItem = document.createElement('li');
+      listItem.innerHTML = currentTask;
+      document.getElementById('taskList').appendChild(listItem);
+      this.setState({word:""});
+      document.getElementById("taskName").reset();
+    }
+
   render() {
     return (
       <div>
-        {this.state.list}
-        <input onChange={(event) =>{this.changeHandler(event);}}/>
-        <button onClick={() =>{this.setState( { list : this.state.word, value: ""} ) } }>Erase</button>
+        <h1> Add Task: </h1>
+          <input type ="text" id ="taskName" placeholder="Add task" onChange={() =>{this.getInputValue(event);}} value = {this.state.word}/>
+          <div className = "submit-btn">
+            <button type ="button" onClick={() =>{this.handleClick(event);}}>Submit</button>
+          </div>
+          <div className = "tasks">
+          <h2> My Tasks:</h2>
+            <ul id = "taskList">
+            </ul>
+          </div>
       </div>
     );
-  };
-};
+  }
+}
 
 export default hot(module)(App);
